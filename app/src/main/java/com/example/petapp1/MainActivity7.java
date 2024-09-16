7a495f425ff2c74ca9555a58df1c8013e4daac5d
HELMAN <helmandacuma5@gmail.com>
2024-09-16 15:41:19 +0800

    Sige

--- app/src/main/java/com/example/petapp1/MainActivity7.java
+++ app/src/main/java/com/example/petapp1/MainActivity7.java
@@ -3,6 +3,7 @@ package com.example.petapp1;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
@@ -33,5 +34,15 @@ public class MainActivity7 extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        Button button = findViewById(R.id.view_profile_button_1);
+// Set an OnClickListener for the button
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity7.this, MainActivity8.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
